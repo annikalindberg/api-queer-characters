8c68748b29743f97662b2ad28142c65c9081abc6
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-08-15 13:19:29 +0200

    first route working

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 import express from "express";
 import cors from "cors";
+import queerCharacters from "./queer-characters";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Define your router
+const router = express.Router();
+
+// Define routes on the router
+router.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+router.get("/api/characters", (req, res) => {
+  const { platform } = req.query;
+  let charactersFromPlatform = queerCharacters;
+
+  if (platform) {
+    charactersFromPlatform = charactersFromPlatform.filter(
+      (character) => character.platform === platform
+    );
+  }
+
+  res.json(charactersFromPlatform); // this is the response that the client will receive when they make a GET request to /api/characters (or /api/characters?platform=Netflix)
+});
+
+// Use the router under the /api path
+app.use('/', router);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 // Start the server
