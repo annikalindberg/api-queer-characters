8ff02e37ba5012f226c8fea3cce8a3c8642c90f1
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-08-15 14:47:07 +0200

    fixed

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-import express, { response } from "express";
+import express from "express";
 import cors from "cors";
-<<<<<<< HEAD
 import queerCharacters from "./queer-characters";
 
 const port = process.env.PORT || 8080;
-=======
-import topMusicData from "./data/top-music.json";
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 9090;
->>>>>>> 40c04fd3003a8a927b9857754037b545504476f0
 const app = express();
-const listEndpoints = require("express-list-endpoints");
 
 app.use(express.json());
 app.use(cors());
 
-<<<<<<< HEAD
 // Define your router
 const router = express.Router();
 
@@ -47,65 +36,10 @@ app.use('/', router);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Internal Server Error" });
-=======
-// Middleware to handle 404 errors
-const notFound = (req, res, next) => {
-  res.status(404).send("Not found");
-};
-
-// Start defining your routes here
-
-app.get("/topmusic", (req, res) => {
-  res.json(topMusicData);
->>>>>>> 40c04fd3003a8a927b9857754037b545504476f0
 });
-
-app.get("/", (req, res) => {
-    res.json(listEndpoints(app));
-  });            
-
-//endpoint that returns all data from the topMusicData array
-app.get("/topmusic/:artist", (req, res) => {
-  const artist = req.params.artist;
-
-//returns all data from the topMusic-route filtered by artist "http://localhost:9090/topmusic/EdSheeran"
-  const filteredArtist = topMusicData.filter((song) => song.artistName === artist);
-  res.json(filteredArtist);
-});
-
-//endpoint that returns all data from topMusic-route based on bpm of the song "http://localhost:9090/topmusic/bpm/100"
-app.get("/topmusic/bpm/:bpm", (req, res) => {
-  const bpm = +req.params.bpm;
-  const bpmFilter = topMusicData.filter((song) => song.bpm >= bpm);
-  res.json(bpmFilter);
-});
-
-
-//endpoint to get single song by id "http://localhost:9090/topmusic/id/1"
-app.get("/topmusic/id/:id", (req, res) => {
-  const id = +req.params.id;
-  const songId = topMusicData.find((song) => song.id === id);
-
-  if (songId) {
-    res.json(songId);
-  } else {
-    // If songId is undefined (i.e. there's no match), call the notFound middleware
-    notFound(req, res, next);
-  }
-});
-
-// get songs by genre "http://localhost:9090/topmusic/genre/pop"
-app.get("/topmusic/genre/:genre", (req, res) => {
-  const genre = req.params.genre.toLowerCase(); // convert to lowercase
-  const genreFilter = topMusicData.filter((song) => song.genre.toLowerCase() === genre); // ignore case-sensitivity
-  res.json({ genreFilter });
-});
-
-
-// Use notFound middleware to handle any other routes that haven't matched yet
-app.use(notFound);
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
