af801b3f44ee9cb0c6698d75d7cd8c3a917d30cf
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-08-15 20:04:10 +0200

    modified endpoint to handle more than 1 platform

--- server.js
+++ server.js
@@ -8,29 +8,44 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
+// Define routes on the router
+/* router.get("/", (req, res) => {
+  res.send("Hello, world!");
+}); */
+
+// Create the platform to characters mapping
+const platformToCharactersMap = {};
+
+// Populate the mapping using the queerCharacters data
+queerCharacters.forEach(character => {
+  const { platform } = character;
+  if (!platformToCharactersMap[platform]) {
+    platformToCharactersMap[platform] = [];
+  }
+  platformToCharactersMap[platform].push(character);
+});
+
 // Define your router
 const router = express.Router();
 
-// Define routes on the router
-router.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
 router.get("/api/characters", (req, res) => {
   const { platform } = req.query;
-  let charactersFromPlatform = queerCharacters;
-
-  if (platform) {
-    charactersFromPlatform = charactersFromPlatform.filter(
-      (character) => character.platform === platform
-    );
-  }
+  let charactersFromPlatform = platformToCharactersMap[platform] || [];
 
   res.json(charactersFromPlatform); // this is the response that the client will receive when they make a GET request to /api/characters (or /api/characters?platform=Netflix)
 });
 
 // Use the router under the /api path
 app.use('/', router);
+
+ /*  if (platform) {
+    charactersFromPlatform = charactersFromPlatform.filter(
+      (character) => character.platform === platform
+    );
+  }
+ */
 
 // Error handling middleware
 app.use((err, req, res, next) => {
